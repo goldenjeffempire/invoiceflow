02bdebb478c678155651a65458fdf7bec242cfe5
goldenjeffempire <jeffemuodafe124@gmail.com>
2026-01-16 00:28:58 +0100

    Authentication & Roles

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('standard', 'Standard User'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='standard')
+    
+    def is_admin(self):
+        return self.role == 'admin'
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm, LoginForm
+from django.core.exceptions import PermissionDenied
+from .decorators import admin_required
 
-# Create your views here.
+
+def signup_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard:home')
+    form = SignupForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect('dashboard:home')
+    return render(request, 'auth/signup.html', {'form': form})
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard:home')
+    form = LoginForm(request, data=request.POST or None)
+    if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return redirect('dashboard:home')
+    return render(request, 'auth/login.html', {'form': form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('auth:login')
+
+@admin_required
+def admin_required(view_func):
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated or not request.user.is_admin():
+            raise PermissionDenied()
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
